c512993628cbeb31ce3d802fab62df0c272c74b1
Ademola Ademeso <desmondademeso@gmail.com>
2024-08-04 23:04:10 +0100

    Created 404 errors handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 """Integrates with AirBnB static HTML Template"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns JSON response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
